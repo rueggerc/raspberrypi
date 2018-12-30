585735f2e9435228ed95812f28949322825ebc72
Chris <chris.ruegger@gmail.com>
2018-12-30 18:23:58 -0500

    Dev

--- kafka/Producer.py
+++ kafka/Producer.py
@@ -1,6 +1,9 @@
 
 from kafka import KafkaConsumer, KafkaProducer
 import time
+import socket
+import sys
+
 
 def publish_message(producer_instance, topic_name, key, value):
     print ('publish message=' + value)
@@ -18,7 +21,8 @@ def publish_message(producer_instance, topic_name, key, value):
 def connect_kafka_producer():
     _producer = None
     try:
-        _producer = KafkaProducer(bootstrap_servers=['hp1:9092'], api_version=(0, 10))
+        brokers = "captain:9092,godzilla:9092,oscar:9092,darwin:9092"
+        _producer = KafkaProducer(bootstrap_servers=brokers, api_version=(0, 10))
         print("Got Producer")
     except Exception as ex:
         print('Exception while connecting Kafka')
@@ -26,12 +30,18 @@ def connect_kafka_producer():
     finally:
         return _producer
 
-topic = "foobar1"
-key = 'sensor2'
-value = "Hello From Python on Raspberry PI"   
+
+numParms = len(sys.argv)
+for x in range(numParms):
+    print("Next Parm=" + sys.argv[x])
+
+host = socket.gethostname()
+topic = "junk"
+key = host
+value = "Hello From Python on " + host   
 
 print('Here we go====')
 kafka_producer = connect_kafka_producer()
-for x in range(10):
-  publish_message(kafka_producer, topic, 'key', value)
-  time.sleep(1)
+while True:
+  publish_message(kafka_producer, topic, key, value)
+  time.sleep(10)
