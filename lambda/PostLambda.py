cb51788d88284a1d4c405e1ddd1d579632abacde
Chris <chris.ruegger@gmail.com>
2019-07-07 13:27:26 -0400

    python3.5

--- lambda/PostLambda.py
+++ lambda/PostLambda.py
@@ -5,7 +5,7 @@ import time
 import sys
 
 def postToLambda(baseURL):
-    print(f'Post To Lambda BEGIN');
+    print('Post To Lambda BEGIN');
     hostname = socket.gethostname()
     stage = 'dev'
     timestamp = int(round(time.time() * 1000))
@@ -18,15 +18,20 @@ def postToLambda(baseURL):
     }
 
     try:
-        lambdaURL = f'{baseURL}/{stage}/sensors/{hostname}/collect'
+        #lambdaURL = f'{baseURL}/{stage}/sensors/{hostname}/collect'
+        lambdaURL = '{}/{}/sensors/{}/collect'.format(baseURL,stage,hostname)
+        print("lambdaURL=" + lambdaURL)
+        #lambdaURL = '{},{:#5.2f},{:#5.2f},{}'.format(host,temperature,humidity,millis,timestamp)
         headers = {'content-type': 'application/json'}
         response = requests.post(lambdaURL,json=payload,headers=headers)
-        print(f'StatusCode={response.status_code}')
-        print(f'ResponseText={response.text}')
+        #print(f'StatusCode={response.status_code}')
+        print('StatusCode={}'.format(response.status_code))
+        print('ResponseText={}'.format(response.text))
+        #print(f'ResponseText={response.text}')
     except Exception as ex:
-        print(f'Exception in POSTing To Lambda:str({ex})')
+        print('Exception in POSTing To Lambda:{}.format(ex)')
         
-    print(f'Post To Lambda END');
+    print('Post To Lambda END');
     
         
 def main():
